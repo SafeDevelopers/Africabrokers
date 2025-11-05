7c6e201051d525d940dd8fc16e10cc7df567bb2a
Fantu Deldil <fmcell16@gmail.com>
2025-11-05 04:41:18 +0300

    fix: Set core-api default port to 3000 and add PORT env var in Dockerfile

--- services/core-api/src/main.ts
+++ services/core-api/src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
-  const port = process.env.PORT || 4000;
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule, {
     rawBody: true, // Enable raw body for webhook signature verification
   });
